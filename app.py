84f51bcc11b9b8826307a0bcd4dc4e66efc08115
vivian5285 <wangjiali240@gmail.com>
2026-07-02 22:15:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def webhook():
 
 @app.route('/health', methods=['GET'])
 def health():
-    return jsonify({"service": "binance_webhook", "status": "ok", "version": "v13.4.8-tp-radar-dingtalk"}), 200
+    return jsonify({"service": "binance_webhook", "status": "ok", "version": "v13.4.10-recover-dingtalk"}), 200
 
 
 if __name__ == '__main__':
