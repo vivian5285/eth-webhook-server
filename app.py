133e8c413fb7ba22d33bb75c5c38d72f7be10696
vivian5285 <wangjiali240@gmail.com>
2026-07-06 02:35:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def webhook():
 
 @app.route('/health', methods=['GET'])
 def health():
-    return jsonify({"service": "binance_webhook", "status": "ok", "version": "v13.5.3-radar-guardian"}), 200
+    return jsonify({"service": "binance_webhook", "status": "ok", "version": "v13.5.5-scorch-verify"}), 200
 
 
 if __name__ == '__main__':
